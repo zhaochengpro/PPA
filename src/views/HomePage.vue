f7e2bf7c8edcf461f34b0c31fbe28c3a286f0979
zhaochengpro <38267662+zhaochengpro@users.noreply.github.com>
2023-02-11 10:38:12 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,11 +2,11 @@
   <Header />
   <div class="video_content">
     <video loop autoplay preload="auto" id="ppa-video" muted>
-      <source src="@/assets/index.mp4" type="video/mp4"/>
+      <source src="@/assets/index.mp4" type="video/mp4" />
     </video>
 
     <audio loop autoplay id="ppa-audio">
-      <source src="@/assets/bg.mp3" type="audio/mpeg"/>
+      <source src="@/assets/bg.mp3" type="audio/mpeg" />
     </audio>
   </div>
 </template>
@@ -30,17 +30,22 @@ function toAuction() {
 onMounted(() => {
   // document.getElementById("ppa-video").muted = false;
   window.localStorage.setItem("isMute", false)
-  isMute.value = window.localStorage.getItem("isMute") == null ? false :  eval(window.localStorage.getItem("isMute"))
-  console.log("xxx", window.localStorage.getItem("isMute") == null ? false :  eval(window.localStorage.getItem("isMute")))
-  setTimeout(() => {
-
-  }, 1000)
+  isMute.value = window.localStorage.getItem("isMute") == null ? false : eval(window.localStorage.getItem("isMute"))
+  console.log("xxx", window.localStorage.getItem("isMute") == null ? false : eval(window.localStorage.getItem("isMute")))
 
   // document.getElementById("ppa-video").muted = true;
-  //   document.getElementById("ppa-video").muted = false;
+  let audio = document.getElementById("ppa-audio");
+  audio.oncanplay = function () {
+    console.log("canplay")
+    audio.play();
+  }
 
-  
+  audio.onplay = () => {
+    console.log("playing")
+  }
 })
+
+
 
 bus.on("toggleMute", (val) => {
   isMute.value = val;
@@ -55,4 +60,5 @@ bus.on("toggleMute", (val) => {
 </script>
 
 <style lang="scss" scoped>
+
 </style>
